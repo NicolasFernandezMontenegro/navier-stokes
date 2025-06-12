bd2e93e2b755d8c25d99567c3657a0ae8103665a
Lisandroallio <lisandro.allio@mi.unc.edu.ar>
2025-06-12 16:39:03 -0300

    asdasdasd

--- navierstokes-gpu/headless.cu
+++ navierstokes-gpu/headless.cu
@@ -115,35 +115,85 @@ static int allocate_data ( void )
 
 
 
-static void react ( float * d, float * u, float * v )
-{
-	int i, size = (N+2)*(N+2);
-	float max_velocity2 = 0.0f;
-	float max_density = 0.0f;
+__global__ void compute_velocity_squared(int size, const float* u, const float* v, float* velocity2) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < size) {
+        float ui = u[i];
+        float vi = v[i];
+        velocity2[i] = ui * ui + vi * vi;
+    }
+}
 
-	max_velocity2 = max_density = 0.0f;
-	for ( i=0 ; i<size ; i++ ) {
-		if (max_velocity2 < u[i]*u[i] + v[i]*v[i]) {
-			max_velocity2 = u[i]*u[i] + v[i]*v[i];
-		}
-		if (max_density < d[i]) {
-			max_density = d[i];
-		}
-	}
+__global__ void clear_arrays_kernel(int size, float* u, float* v, float* d) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < size) {
+        u[i] = 0.0f;
+        v[i] = 0.0f;
+        d[i] = 0.0f;
+    }
+}
 
-	for ( i=0 ; i<size ; i++ ) {
-		u[i] = v[i] = d[i] = 0.0f;
-	}
+__global__ void inject_center_kernel(float* u, float* v, float* d,
+                                     float max_velocity2, float max_density,
+                                     float force, float source, int center) {
+    if (threadIdx.x == 0 && blockIdx.x == 0) {
+        if (max_velocity2 < 0.0000005f) {
+            float val = force * 10.0f;
+            u[center] = val;
+            v[center] = val;
+        }
+        if (max_density < 1.0f) {
+            float val = source * 10.0f;
+            d[center] = val;
+        }
+    }
+}
 
-	if (max_velocity2<0.0000005f) {
-		u[IX(N/2,N/2)] = force * 10.0f;
-		v[IX(N/2,N/2)] = force * 10.0f;
-	}
-	if (max_density<1.0f) {
-		d[IX(N/2,N/2)] = source * 10.0f;
-	}
+static void react(float* d, float* u, float* v) {
+    const int size = (N + 2) * (N + 2);
+    dim3 block(128);
+    dim3 grid((size + block.x - 1) / block.x);
 
-	return;
+    float* d_velocity2;
+    checkCudaCall(cudaMalloc(&d_velocity2, size * sizeof(float)));
+
+
+    compute_velocity_squared<<<grid, block>>>(size, u, v, d_velocity2);
+    checkCudaCall(cudaGetLastError());
+	checkCudaCall(cudaDeviceSynchronize());
+
+
+    void* temp_storage = nullptr;
+    size_t temp_storage_bytes = 0;
+    checkCudaCall(cub::DeviceReduce::Max(nullptr, temp_storage_bytes, d_velocity2, d_velocity2, size));
+    checkCudaCall(cudaMalloc(&temp_storage, temp_storage_bytes));
+    checkCudaCall(cub::DeviceReduce::Max(temp_storage, temp_storage_bytes, d_velocity2, d_velocity2, size));
+
+    float* d_max_density;
+    checkCudaCall(cudaMalloc(&d_max_density, sizeof(float)));
+    void* temp_storage2 = nullptr;
+    size_t temp_storage_bytes2 = 0;
+    checkCudaCall(cub::DeviceReduce::Max(nullptr, temp_storage_bytes2, d, d_max_density, size));
+    checkCudaCall(cudaMalloc(&temp_storage2, temp_storage_bytes2));
+    checkCudaCall(cub::DeviceReduce::Max(temp_storage2, temp_storage_bytes2, d, d_max_density, size));
+
+    float max_velocity2;
+    float max_density;
+    checkCudaCall(cudaMemcpy(&max_velocity2, d_velocity2, sizeof(float), cudaMemcpyDeviceToHost));
+    checkCudaCall(cudaMemcpy(&max_density, d_max_density, sizeof(float), cudaMemcpyDeviceToHost));
+
+    clear_arrays_kernel<<<grid, block>>>(size, u, v, d);
+    checkCudaCall(cudaGetLastError());
+
+    int center = IX(N / 2, N / 2);
+    inject_center_kernel<<<1, 1>>>(u, v, d, max_velocity2, max_density, force, source, center);
+    checkCudaCall(cudaGetLastError());
+	checkCudaCall(cudaDeviceSynchronize());
+
+    cudaFree(d_velocity2);
+    cudaFree(d_max_density);
+    cudaFree(temp_storage);
+    cudaFree(temp_storage2);
 }
 
 static void one_step ( void )
