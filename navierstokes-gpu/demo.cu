2e01080b6087046d019d46dc8b62382d1d4362db
Lisandroallio <lisandro.allio@mi.unc.edu.ar>
2025-06-12 17:55:26 -0300

    sdadasd

--- navierstokes-gpu/demo.cu
+++ navierstokes-gpu/demo.cu
@@ -249,8 +249,6 @@ static void react(float* d, float* u, float* v) {
 
     compute_velocity_squared<<<grid, block>>>(size, u, v, d_velocity2);
     checkCudaCall(cudaGetLastError());
-	checkCudaCall(cudaDeviceSynchronize());
-
 
     void* temp_storage = nullptr;
     size_t temp_storage_bytes = 0;
@@ -277,7 +275,6 @@ static void react(float* d, float* u, float* v) {
     int center = IX(N / 2, N / 2);
     inject_center_kernel<<<1, 1>>>(u, v, d, max_velocity2, max_density, force, source, center);
     checkCudaCall(cudaGetLastError());
-	checkCudaCall(cudaDeviceSynchronize());
 
     if (!mouse_down[0] && !mouse_down[2]) {
         cudaFree(d_velocity2);
@@ -381,6 +378,8 @@ static void idle_func ( void )
 	start_t = wtime();
 	dens_step ( N, dens, dens_prev, u, v, diff, dt );
 	dens_ns_p_cell += 1.0e9 * (wtime()-start_t)/(N*N);
+
+	cudaDeviceSynchronize();
 
 	if (1.0<wtime()-one_second) { /* at least 1s between stats */
 		printf("%lf, %lf, %lf, %lf: ns per cell total, react, vel_step, dens_step\n",
